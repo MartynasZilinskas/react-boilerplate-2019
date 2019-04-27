3a32d8919ad083e60611fa57dd7715ff619f3d6b
Martynas Žilinskas <martiogalaltu@gmail.com>
2019-04-27 16:18:26 +0300

    Project init. (#1)

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import "@babel/polyfill";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/main.scss";
+
+ReactDOM.render(<div>Hello World!</div>, document.getElementById("root"));
